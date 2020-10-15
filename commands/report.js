d3b967d6cd43e2a0828342bbafbadecf3d22eb9e
Camdan Mead <camthehelpdesk@gmail.com>
2020-10-15 15:02:12 -0600

    fix: fix function with too many lines
    FIXES #6

--- commands/report.js
+++ commands/report.js
@@ -6,8 +6,10 @@ const reportedUserEmbed = require('./functions/report/embeds/reporteduser.js');
 const goby = require('goby');
 
 const chalk = require('chalk');
+const chalkWarning = chalk.bold.yellow;
 const chalkSuccess = chalk.green;
 const chalkCommandRun = chalk.bgGreen.bold;
+const chalkCommandFail = chalk.bgRed.bold;
 
 module.exports = {
 	name: 'report',
@@ -19,47 +21,55 @@ module.exports = {
 	guildOnly: true,
 	execute(message, args) {
 		if (message.mentions.users.size) {
-			const reportedUser = message.mentions.users.first();
-
-			const member = message.guild.member(reportedUser);
-			const nickname = member ? member.displayName : null;
-
-			getToxicity(nickname).then(toxicity => {
-				if (toxicity > 0.90) {
-					const newNickname = goby.init().generate(['adj', 'pre', 'suf']);
-					member.setNickname(newNickname);
-					message.author.send(`**HEADS UP:** The user you reported, \`${nickname}\` from \`${message.guild.name}\` was renamed \`${newNickname}\`. Thank you for reporting and keeping the server clean! (The nickname was found to be **toxic** with **${toxicity * 100}%** certianty.)`);
-					reportedUserEmbed(message, nickname, newNickname, reportedUser, toxicity);
-				}
-				else {
-					message.author.send(`**HEADS UP:** The user you reported, \`${nickname}\` from \`${message.guild.name}\` had no action taken against them because our machine learning algorythms determined that their username was **not an issue** with **${100 - (toxicity * 100)}%** certianty. If you feel this is an error, please alert the server's mods. **NO ACTION HAS BEEN TAKEN.**`);
-				}
-				console.log(`[USER REPORTED] NAME ${nickname} - SCORE ${toxicity}`);
-				message.delete();
-			});
+			reportUser(message);
 		}
 		else if (!isNaN(parseInt(args[0]))) {
-			const reportedMessageID = args[0];
-			message.channel.messages.fetch(reportedMessageID).then(reportedMessage => {
-				getToxicity(reportedMessage.content).then(toxicity => {
-					if (toxicity > 0.90) {
-						message.channel.messages.fetch(reportedMessageID).then(deleteMessage => {
-							deleteMessage.delete();
-							message.author.send(`**HEADS UP:** The message you reported, \`${reportedMessage.content}\` was deleted from **${reportedMessage.guild.name}**. Thank you for reporting and keeping the server clean! (The message was found to be **toxic** with **${toxicity * 100}%** certianty.)`);
-							reportedMessageAuthorEmbed(reportedMessage, toxicity);
-						});
-					}
-					else {
-						message.author.send(`**HEADS UP:** The message you reported, \`${reportedMessage.content}\` in **${reportedMessage.guild.name}** was not deleted because our machine learning algorythms determined that the message was **not an issue** with **${100 - (toxicity * 100)}%** certianty. If you feel this is an error, please alert the server's mods. **NO ACTION HAS BEEN TAKEN.**`);
-					}
-					console.log(chalkCommandRun('[MESSAGE REPORTED]') + chalkSuccess(` MESSAGE ${reportedMessage.content} - SCORE ${toxicity}`));
-					message.delete();
-				});
-			});
+			reportMessage(message, args);
 		}
 		else {
+			console.log(chalkCommandFail('[REPORT FAIL]') + chalkWarning(` INVALID ARGS - USER ${message.author.tag} - ARGS ${args}`));
 			message.channel.send('You need to mention a user or provide a message ID!');
 		}
-
 	},
 };
+
+function reportUser(message) {
+	const reportedUser = message.mentions.users.first();
+
+	const member = message.guild.member(reportedUser);
+	const nickname = member ? member.displayName : null;
+
+	getToxicity(nickname).then(toxicity => {
+		if (toxicity > 0.90) {
+			const newNickname = goby.init().generate(['adj', 'pre', 'suf']);
+			member.setNickname(newNickname);
+			message.author.send(`**HEADS UP:** The user you reported, \`${nickname}\` from \`${message.guild.name}\` was renamed \`${newNickname}\`. Thank you for reporting and keeping the server clean! (The nickname was found to be **toxic** with **${toxicity * 100}%** certianty.)`);
+			reportedUserEmbed(message, nickname, newNickname, reportedUser, toxicity);
+		}
+		else {
+			message.author.send(`**HEADS UP:** The user you reported, \`${nickname}\` from \`${message.guild.name}\` had no action taken against them because our machine learning algorythms determined that their username was **not an issue** with **${100 - (toxicity * 100)}%** certianty. If you feel this is an error, please alert the server's mods. **NO ACTION HAS BEEN TAKEN.**`);
+		}
+		console.log(`[USER REPORTED] NAME ${nickname} - SCORE ${toxicity}`);
+		message.delete();
+	});
+}
+
+function reportMessage(message, args) {
+	const reportedMessageID = args[0];
+	message.channel.messages.fetch(reportedMessageID).then(reportedMessage => {
+		getToxicity(reportedMessage.content).then(toxicity => {
+			if (toxicity > 0.90) {
+				message.channel.messages.fetch(reportedMessageID).then(deleteMessage => {
+					deleteMessage.delete();
+					message.author.send(`**HEADS UP:** The message you reported, \`${reportedMessage.content}\` was deleted from **${reportedMessage.guild.name}**. Thank you for reporting and keeping the server clean! (The message was found to be **toxic** with **${toxicity * 100}%** certianty.)`);
+					reportedMessageAuthorEmbed(reportedMessage, toxicity);
+				});
+			}
+			else {
+				message.author.send(`**HEADS UP:** The message you reported, \`${reportedMessage.content}\` in **${reportedMessage.guild.name}** was not deleted because our machine learning algorythms determined that the message was **not an issue** with **${100 - (toxicity * 100)}%** certianty. If you feel this is an error, please alert the server's mods. **NO ACTION HAS BEEN TAKEN.**`);
+			}
+			console.log(chalkCommandRun('[MESSAGE REPORTED]') + chalkSuccess(` MESSAGE ${reportedMessage.content} - SCORE ${toxicity}`));
+			message.delete();
+		});
+	});
+}
